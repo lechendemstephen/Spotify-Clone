2ac725676acac502076c52d9a759f8eb650259d0
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2025-02-20 13:57:12 -0500

    working morr on the frontend

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const DisplayAlbum = () => {
+  return (
+    <div>
+      <Navbar/> 
+    </div>
+  )
+}
+
+export default DisplayAlbum
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; // Import Tailwind CSS
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+
     <App />
+    
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
